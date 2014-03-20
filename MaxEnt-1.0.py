683f4194096e17a5c789cbe1f14e3edebc462477
Melchor Sanchez <melchor.sanchez@iqac.csic.es>
2014-03-20 18:49:35 +0100

    MaxEnt-1.0 and run_pales-1.0 release

---
+++ MaxEnt-1.0.py
@@ -0,0 +1,182 @@
+"""
+A maximum entropy implementation to reproduce RDCs
+Giving the experimental and calcultes RDCsd values
+and temperature data, calculates the exact weights
+(and threshold) by reweighting
+"""
+import sys, io
+import numpy as np 
+import pylab as plt
+import scipy.optimize as so
+import argparse
+plt.ion()
+
+def qave(lam, q):
+    """
+    return the 'corrected' experimental RDCs values
+    """
+    f = np.exp(np.dot(-q,lam))
+    return np.dot(f,q)
+
+def w(lam, q):
+    """
+    return the weights
+    """
+    w = np.exp(np.dot(-q,lam))
+    w /= w.sum()
+    return w
+
+def grad_fit_rmsd2(lam, q, Q, thres, k):
+    """
+    return the gradient of fit_rmsd2 with respect to lam
+    """
+    # Gradient of qave (each row i is dq/dlambda_i)
+    dq=-np.tensordot(q,(np.exp(np.dot(-q,lam))*q.T).T, axes=[0,0])
+    # gradient of factq
+    qa = qave(lam,q)
+    qq = np.dot(qa,qa)
+    qQ = np.dot(qa,Q)
+    s = np.sign(qQ)
+    dfactq = s*np.dot(dq,Q)*qq-2*np.abs(np.dot(Q,qa))*np.dot(dq,qa)
+    dfactq /= qq*qq
+    #gradient of fit_rmsd2
+    # factor coming from f1
+    factq = np.abs(qQ)/qq
+    vec = qa*factq - Q
+    f1 = np.dot(vec, vec)/len(Q)
+    if f1>thres*thres:
+        df1 = 2*np.dot(np.outer(dfactq,qa)+factq*dq, factq*qa-Q)
+    else:
+        df1 = np.zeros_like(lam)
+    # factor coming from f2
+    df2 = 2*k*lam
+    return (df1+df2)/len(Q)
+
+def fit_rmsd2(lam, q, Q, thres,k):
+    """
+    Return the threshold value achieved
+    The average diffenrence between the reweighted values
+    and the previous ones
+    """
+    qa = qave(lam, q)
+    factq = np.abs(np.dot(Q, qa))/np.dot(qa, qa)
+    vec = qa*factq - Q
+    f1 = np.dot(vec, vec)
+    if f1<thres*thres : f1 = thres*thres
+    f2 = k*np.dot(lam, lam)
+    return (f1+f2)/len(Q)
+
+def rmsd(lam, q, Q):
+    """
+    Return the RMSD between experiental and calculated RDCs
+    """
+    curr_qave = qave(lam, q)
+    factq = np.abs(np.dot(Q, curr_qave))/np.dot(curr_qave, curr_qave)
+    vec = curr_qave*factq - Q
+    return np.sqrt(np.dot(vec, vec)/len(Q))
+
+
+#Defining the arguments:
+argparser = argparse.ArgumentParser(description="Maximum Entropy. Fit calculate RDCs to experimental RDCs")
+data=argparser.add_argument_group("Initial_data", "Entry the files that contain the RDCs to evaluate.")
+data.add_argument("--calculated", "-c", help="Calculated RDCs")
+data.add_argument("--experimental", "-e", help = "Experimental RDCs")
+data.add_argument("--save", "-s", choices=["dat","txt","npy"], default=False, help="Save the Optimized RDCs")
+data.add_argument("--save_weights", "-sw", choices=["dat","txt","npy"], default=False, help="Save the Optimized weights")
+
+
+residues=argparser.add_argument_group("residues", "Range of residues represnted by the RDCs")
+residues.add_argument("--initial_residue", "-r", help = "Initial residue")
+residues.add_argument("--final_residue", "-f", help = "Final residue")
+
+
+global args
+args = argparser.parse_args()
+
+k = 10000000. 
+
+if args.experimental: # Experimental RDCs data is loaded. 
+    type = args.experimental.split(".")[-1]
+    if type == "txt" or type == "dat" :
+        Q=np.loadtxt(args.experimental)
+    elif type == "npy" or type == "npz":
+        Q=np.load(args.experimental)        
+resind = Q[:, 0]
+Q = Q[:, 1]
+
+if args.calculated:#  Calculated RDCs data is loaded. 
+    type = args.calculated.split(".")[-1]
+    if type == "txt"or type == "dat" :
+        q=-np.loadtxt(args.calculated)
+    elif type == "npy" or type == "npz":
+        q=-np.load(args.calculated)
+
+residues = np.arange(int(args.initial_residue), (int(args.final_residue)+1))
+residues = np.asarray(residues)
+q = q[:, residues]	
+
+lam = np.zeros(len(Q)) #Lambda initialization
+# Generate initial plot
+fig = plt.figure(figsize=(11, 6))
+ax = fig.add_subplot(121)
+ax.set_xlim(int(args.initial_residue), int(args.final_residue))
+ref, = ax.plot(residues, Q, 'o-')
+
+
+#The maximum agreement we can expect is the same as for reweighting
+threshold = 1.0
+
+#add the unscaled values
+qnew = qave(lam, q)
+factq = np.abs(np.dot(Q, qnew))/np.dot(qnew, qnew)
+ax.plot(residues, factq*qnew, 'x-')
+axw = fig.add_subplot(122)
+line, = ax.plot(residues, factq*qnew, 'o-')
+
+
+#Plot Weights
+wplot, = axw.semilogy(np.ones(len(q)), '-')
+axw.set_ylim(0.01,100)
+plt.draw()
+
+#Minimize reducing k until threshold is reached
+fit = rmsd(lam, q, Q)
+print ("="*50)
+print (" "*15, " Maximum Entropy ")
+print ("="*50)
+print ("%9s %12s %18s " %('Fit','k','Lambda'))
+print
+print ("="*50)
+print("{:10.3f} {:15.1f} {:15.3e}".format(fit, k, 0.0))
+
+first_time=True
+while True:
+    lam = so.fmin_ncg(fit_rmsd2, lam, fprime=grad_fit_rmsd2,
+          args=(q, Q, threshold,k), disp=False, epsilon = 1e-10)
+    fit = rmsd(lam, q, Q)
+    qnew = qave(lam, q)
+    factq = np.abs(np.dot(Q, qnew))/np.dot(qnew, qnew)
+    line.set_ydata(factq*qnew)
+    wplot.set_ydata(np.sort(w(lam,q)*len(q)))
+    avelam = np.sqrt(np.dot(lam, lam)/len(lam))
+    print("{:10.3f} {:15.1f} {:15.3e}" .format(fit, k, avelam))
+    plt.draw()
+    if first_time and fit < threshold*1.05:
+        lam *= 0.0
+        k *= 1.5
+    elif fit > threshold*1.05 :
+        first_time=False 
+        k *= 0.8
+    if not first_time and fit < threshold*1.05: break
+
+# Save Optimized RDCS
+if args.save == "txt" or args.save == "dat":
+    np.savetxt('./Optimized_RDCs.'+args.save, qnew)
+if args.save == "npy" :           
+    np.save('./Optimized_RDCs', qnew)    
+#Save Optimized weights
+if args.save_weights == "txt" or args.save == "dat":
+    np.savetxt('./Optimized_weights.'+args.save, np.sort(w(lam,q)))
+if args.save_weights == "npy" :           
+    np.save('./Optimized_weights', np.sort(w(lam,q)))
+input()
---
+++ run_pales-1.0.py
@@ -0,0 +1,55 @@
+"""
+Run pales-linux to generate all RDCs from the PDBs of a directory.
+The -H option determines which option to use when calling pales
+"""
+import glob, sys, os
+import subprocess as subp
+import argparse
+
+def createPath(path):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+def generate_rdcs_files(pales_path, initial_structure, initial_dipolar_coupling, dipolar_coupling_output, H):
+    fileout =  open("/dev/null", 'w')
+    if args.H:
+        pales_call = '%s -pdb %s -inD %s -outD %s.rdcs -H' % (pales_path, initial_structure, initial_dipolar_coupling, dipolar_coupling_output)
+    else:
+        pales_call = '%s -pdb %s -inD %s -outD %s.rdcs' % (pales_path, initial_structure, initial_dipolar_coupling, dipolar_coupling_output)
+    pales_call = pales_call.split()
+    subp.call(pales_call, stdout = fileout, stderr=subp.STDOUT)
+
+argparser = argparse.ArgumentParser(description="Extract RDC values from .pdb files using the PALES program.")
+data=argparser.add_argument_group("")
+data.add_argument('--path', '-p', help ="The directory where the pales executable is.")
+data.add_argument('--indirectory', '-id', help ="The directory where the pdbs are.")
+data.add_argument('--outdirectory', '-od', help ="The directory where the RDCs outfiles are going to be.")
+data.add_argument('--inD', '-i',  help ="Pales Dipolar Coupling PDB input file. Determine which rdcs to calculate.")
+data.add_argument('--H', action='store_true', help ="Whether to use -H pales option. Enablle comparision of all PDB atoms. By default, all except Hydrogens")
+
+global args
+args = argparser.parse_args()
+
+#Checking the Initial options
+
+if not args.path: print('Define the path to the run_pales executable with --path option')
+if args.indirectory:
+    filelist_pdbs = glob.glob(args.indirectory +'/*.pdb') 
+    filelist_pdbs.sort()
+else: print('Define the path to the directory where the PDBs are with --directory option')
+if args.outdirectory: createPath(args.outdirectory)
+else: args.outdirectory=args.indirectory
+if not args.inD: print('Provide the initial RDCS data into a table with --inD option')
+
+#Executing generate_rdc_files. Storing in a .dat file the name of all the processed PDBs in order.
+
+writer=open(args.outdirectory+'Processed_PDB.dat', mode='w')
+print (args.outdirectory)
+print('Generating %i rdcs from %s.'%(len(filelist_pdbs), args.indirectory))
+for i, filename in enumerate(filelist_pdbs):
+    print("\rDoing structure %5d" %(i,), end="")
+    generate_rdcs_files(args.path, filename, args.inD, filename[:-4], args.H)
+    subp.call(['mv', filename[:-4]+'.rdcs', args.outdirectory])
+    writer.write(filename+'\n')
+writer.close()
+print()
