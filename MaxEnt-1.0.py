86ef864719b4cc5f54d0feb380a6345d35725063
Ramon Crehuet <ramon.crehuet@iqac.csic.es>
2014-04-01 13:12:32 +0200

    Update MaxEnt-1.0.py

--- MaxEnt-1.0.py
+++ MaxEnt-1.0.py
@@ -1,18 +1,23 @@
+#!/usr/bin/env python3
 """
 A maximum entropy implementation to reproduce RDCs.
-Giving two sets of RDCs values, optimizes the values 
+Giving two sets of RDCs values, optimizes the values
 of one of the sets respect to the other.
+
+Author: Ramon Crehuet, Melchor Sanchez-Martinez
+
+Date: 03/04/2014
 """
-import sys, io
-import numpy as np 
-import pylab as plt
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
 import scipy.optimize as so
 import argparse
 plt.ion()
 
 def qave(lam, q):
     """
-    return the 'corrected' experimental RDCs values
+    return the expected RDCs values for a given lambas
     """
     f = np.exp(np.dot(-q,lam))
     return np.dot(f,q)
@@ -53,9 +58,7 @@ def grad_fit_rmsd2(lam, q, Q, thres, k):
 
 def fit_rmsd2(lam, q, Q, thres,k):
     """
-    Return the threshold value achieved
-    The average diffenrence between the reweighted values
-    and the previous ones
+    Return the f2 function. See the associated article.
     """
     qa = qave(lam, q)
     factq = np.abs(np.dot(Q, qa))/np.dot(qa, qa)
@@ -67,82 +70,88 @@ def fit_rmsd2(lam, q, Q, thres,k):
 
 def rmsd(lam, q, Q):
     """
-    Return the RMSD between experiental and calculated RDCs
+    Return the RMSD between experiental and calculated RDCs. The f1 function in 
+    the associated article.
     """
-    curr_qave = qave(lam, q)
-    factq = np.abs(np.dot(Q, curr_qave))/np.dot(curr_qave, curr_qave)
-    vec = curr_qave*factq - Q
+    qa = qave(lam, q)
+    factq = np.abs(np.dot(Q, qa))/np.dot(qa, qa)
+    vec = qa*factq - Q
     return np.sqrt(np.dot(vec, vec)/len(Q))
 
 
 #Defining the arguments:
-argparser = argparse.ArgumentParser(description="Maximum Entropy. Fit calculate RDCs to experimental RDCs")
-data=argparser.add_argument_group("Initial_data", "Entry the files that contain the RDCs to evaluate.")
-data.add_argument("--calculated", "-c", help="Second set of RDCs. Entry the file that contains the second set of RDCs. Calculated RDCs.")
-data.add_argument("--experimental", "-e", help = "First set of RDCs. Experimental or Calculated RDCs ensemble to the second set is going to be fitted")
-data.add_argument("--save", "-s", choices=["dat","txt","npy"], default=False, help="Save the Optimized RDCs")
-data.add_argument("--save_weights", "-sw", choices=["dat","txt","npy"], default=False, help="Save the Optimized weights")
-data.add_argument("--save_image", "-si",  choices=["png","jpeg"], default=False, help="Save an image of the Optimized RDCs together with the initial RDCs sets and the optimized weights")
+parser = argparse.ArgumentParser(description="Maximum Entropy fit of ensemble RDCs to experimental RDCs")
+parser.add_argument("calculated", help="RDCs for each structure. A numpy array or a text file of shape (M,N).")
+parser.add_argument("experimental", help = "Experimental RDCs. A numpy array or a text file of shape (2,N), where the first column is the residue number and the second its RDC value")
+parser.add_argument("--save", "-s", \
+    help="Save the Optimized RDCs in text or numpy format (according to extension)")
+parser.add_argument("--save_weights", "-sw", \
+   help="Save the Optimized weights in text or numpy (npy) format (according to extension).")
+parser.add_argument("--save_image", "-si",  \
+   help="Save an image of the Optimized RDCs together with the initial RDCs sets and the optimized weights")
+
+parser.add_argument("--initial_residue", "-i", help = "Initial residue to fit by the RDCs", type=int)
+parser.add_argument("--final_residue", "-f", help = "Final residue to fit by the RDCs", type=int)
 
 
-residues=argparser.add_argument_group("residues", "Range of residues represnted by the RDCs")
-residues.add_argument("--initial_residue", "-r", help = "Initial residue")
-residues.add_argument("--final_residue", "-f", help = "Final residue")
+args = parser.parse_args()
 
+k = 10000000.
 
-global args
-args = argparser.parse_args()
-
-k = 10000000. 
-
-if args.experimental: # Experimental RDCs data is loaded. 
-    type = args.experimental.split(".")[-1]
-    if type == "txt" or type == "dat" :
-        Q=np.loadtxt(args.experimental)
-    elif type == "npy" or type == "npz":
-        Q=np.load(args.experimental)        
+if args.experimental: # Experimental RDCs data is loaded.
+    fileext = args.experimental.split(".")[-1]
+    if fileext == "npy" :
+        Q = np.load(args.experimental)
+    else:
+        Q = np.loadtxt(args.experimental)
 resind = Q[:, 0]
+resind = np.asarray(resind, dtype=np.int)
 Q = Q[:, 1]
 
-if args.calculated:#  Calculated RDCs data is loaded. 
-    type = args.calculated.split(".")[-1]
-    if type == "txt"or type == "dat" :
-        q=-np.loadtxt(args.calculated)
-    elif type == "npy" or type == "npz":
-        q=-np.load(args.calculated)
+if args.calculated:# Calculated RDCs data is loaded.
+    fileext = args.calculated.split(".")[-1]
+    if fileext == "npy" :
+        q = -np.load(args.calculated)
+    else:
+        q = -np.loadtxt(args.calculated)
 
-residues = np.arange(int(args.initial_residue), (int(args.final_residue)+1))
-residues = np.asarray(residues)
+if args.initial_residue:
+    ini = args.initial_residue-1
+else:
+    ini = 0
+if args.final_residue:
+    fin = args.final_residue-1
+else:
+    fin = len(Q)    
+residues = np.arange(int(ini), int(fin)+1)
+
 q = q[:, residues]	
 
 lam = np.zeros(len(Q)) #Lambda initialization
 # Generate initial plot
 fig = plt.figure(figsize=(11, 6))
 ax = fig.add_subplot(121)
-ax.set_xlim(int(args.initial_residue), int(args.final_residue))
-ref, = ax.plot(residues, Q, 'o-')
+ref, = ax.plot(resind, Q, 'o-', label='experimental')
 
-
-#The maximum agreement we can expect is the same as for reweighting
+#The experimental error
 threshold = 1.0
 
 #add the unscaled values
 qnew = qave(lam, q)
 factq = np.abs(np.dot(Q, qnew))/np.dot(qnew, qnew)
-ax.plot(residues, factq*qnew, 'x-')
+ax.plot(resind, factq*qnew, 'x-', label='inital')
 axw = fig.add_subplot(122)
-line, = ax.plot(residues, factq*qnew, 'o-')
-
+line, = ax.plot(resind, factq*qnew, 'o-', label='re-weighted')
 
 #Plot Weights
 wplot, = axw.semilogy(np.ones(len(q)), '-')
-axw.set_ylim(0.01,100)
+axw.set_ylim(0.1,10)
 plt.draw()
 
 #Minimize reducing k until threshold is reached
 fit = rmsd(lam, q, Q)
 print ("="*50)
-print (" "*15, " Maximum Entropy ")
+print (" "*15, " Maximum Entropy Fit")
 print ("="*50)
 print ("%9s %12s %18s " %('Fit','k','Lambda'))
 print
@@ -155,33 +164,41 @@ while True:
           args=(q, Q, threshold,k), disp=False, epsilon = 1e-10)
     fit = rmsd(lam, q, Q)
     qnew = qave(lam, q)
-    factq = np.abs(np.dot(Q, qnew))/np.dot(qnew, qnew)
-    line.set_ydata(factq*qnew)
+    qnew *= np.abs(np.dot(Q, qnew))/np.dot(qnew, qnew)
+    line.set_ydata(qnew)
     wplot.set_ydata(np.sort(w(lam,q)*len(q)))
     avelam = np.sqrt(np.dot(lam, lam)/len(lam))
     print("{:10.3f} {:15.1f} {:15.3e}" .format(fit, k, avelam))
     plt.draw()
-    if first_time and fit < threshold*1.05:
+    if first_time and fit < threshold:
         lam *= 0.0
         k *= 1.5
-    elif fit > threshold*1.05 :
-        first_time=False 
-        k *= 0.8
-    if not first_time and fit < threshold*1.05: break
+    elif fit >= threshold :
+        first_time=False
+        k *= 0.9
+    if not first_time and fit < threshold: break
 
+w_opt = w(lam, q)
+ax.legend(fontsize='small', loc='best')
+axw.set_ylim(10**np.floor(np.log10(np.min(len(q)*w_opt))),10**np.ceil(np.log10(np.max(len(q)*w_opt))))
+plt.show()
 # Save Optimized RDCS
-if args.save == "txt" or args.save == "dat":
-    np.savetxt('./Optimized_RDCs.'+args.save, qnew)
-if args.save == "npy" :           
-    np.save('./Optimized_RDCs', qnew)    
+if args.save:
+    fileext = args.save.split(".")[-1]
+    if fileext =='npy':
+        np.save(args.save, np.c_[resind, qnew])
+    else:
+        np.savetxt(args.save, np.c_[resind,qnew])
 #Save Optimized weights
-if args.save_weights == "txt" or args.save == "dat":
-    np.savetxt('./Optimized_weights.'+args.save, np.sort(w(lam,q)))
-if args.save_weights == "npy" :           
-    np.save('./Optimized_weights', np.sort(w(lam,q)))
+if args.save_weights:
+    fileext = args.save_weights.split(".")[-1]
+    if fileext =='npy':
+        np.save(args.save_weights, w_opt)
+    else:
+        np.savetxt(args.save_weights, w_opt)
+
 #Save Iamge
-if args.save_image == "png":
-	plt.savefig('Final_Optimized_RDCs.png')
-if args.save_image == "jpeg":
-	plt.savefig('Final_Optimized_RDCs.jpeg')    
+if args.save_image:
+    plt.savefig(args.save_image)
+
 input()
